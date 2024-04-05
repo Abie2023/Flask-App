2af8a5893552fbdb608762555fe1dcccf17539cf
unknown <Abie2023>
2024-04-05 17:31:05 +0530

    Update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
+import os
 import save_transaction
 
 app = Flask(__name__)
@@ -30,6 +31,13 @@ def save_transaction_route():
 @app.route('/save_data', methods=['POST'])
 def save_data_route():
     return save_transaction.save(request)
+
+@app.route('/data/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.path.abspath(__file__))  
+    data_dir = os.path.join(root_dir, 'data') 
+    return send_from_directory(data_dir, filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
